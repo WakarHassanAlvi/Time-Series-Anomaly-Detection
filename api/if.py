ae072d2753f78bd4810575cbd28d36b0b9fbf63b
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-08 15:05:36 +0100

    Update CSV path for Heroku

--- api/if.py
+++ api/if.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 import pandas as pd
 from io import StringIO
 import pickle
+import os
 
 
 from pipeline.preprocess import get_preprocessed
@@ -40,6 +41,9 @@ async def create_upload_file_if(sensor_data: UploadFile = File(...)) :
    res['machine_status'] = dframe['machine_status']
    print(res)
 
-   filepath = "./data/uploads/ifr.csv"
+   path_parent = os.path.dirname(os.getcwd())
+   os.chdir(path_parent)
+   cwd = os.getcwd()
+   filepath = cwd + "/data/uploads/ifr.csv"
    res.to_csv(filepath, index=False)
    return {"csv": filepath}
