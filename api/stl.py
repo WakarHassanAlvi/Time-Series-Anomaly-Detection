20fec7f658912bde9af90a9792afb1266a2c4dfa
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-09 16:04:14 +0100

    changes for heroku

--- api/stl.py
+++ api/stl.py
@@ -18,25 +18,24 @@ async def create_upload_file_stl(coef: str, sensor_data: UploadFile = File(...))
    dframe = get_preprocessed(df)
    #define x
    df = dframe.iloc[:, 0:1]
-   print(df)
 
-   # sampled_df = sample_sensor_data(df)
-   # stlData = stl_model(sampled_df)
-   # l, u = get_anomaly_limits(stlData.resid, coef)
-   # anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
-   # anomalies_list = anomalies.values.tolist()
-   # dfsensor = get_indexed_df(sampled_df, 0)
-   # dfsensor_list = dfsensor.values.tolist()
+   sampled_df = sample_sensor_data(df)
+   stlData = stl_model(sampled_df)
+   l, u = get_anomaly_limits(stlData.resid, coef)
+   anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
+   anomalies_list = anomalies.values.tolist()
+   dfsensor = get_indexed_df(sampled_df, 0)
+   dfsensor_list = dfsensor.values.tolist()
 
-   # # #dfsensor, anomalies = stl_decomposition(sensor_data, int(coef))
-   # # anomalies = anomalies.rename({'0': 'sensor_values'}, axis=1).reset_index()
-   # # dfsensor = dfsensor.reset_index()
-   # # filepathAnomalies = "./data/uploads/stl.csv"
-   # # filepathSampledSensorData = "./data/uploads/sampledSensorStl.csv"
+   # #dfsensor, anomalies = stl_decomposition(sensor_data, int(coef))
+   # anomalies = anomalies.rename({'0': 'sensor_values'}, axis=1).reset_index()
+   # dfsensor = dfsensor.reset_index()
+   # filepathAnomalies = "./data/uploads/stl.csv"
+   # filepathSampledSensorData = "./data/uploads/sampledSensorStl.csv"
    
-   # # # to upload files
-   # # anomalies.to_csv(filepathAnomalies, index=False)
-   # # dfsensor.to_csv(filepathSampledSensorData, index=False)
+   # # to upload files
+   # anomalies.to_csv(filepathAnomalies, index=False)
+   # dfsensor.to_csv(filepathSampledSensorData, index=False)
 
-   # # return {"anomaly_csv": filepathAnomalies, "sensor_csv": filepathSampledSensorData}
-   # return {"anomalies": anomalies_list, "sensor": dfsensor_list}
+   # return {"anomaly_csv": filepathAnomalies, "sensor_csv": filepathSampledSensorData}
+   return {"anomalies": anomalies_list, "sensor": dfsensor_list}
