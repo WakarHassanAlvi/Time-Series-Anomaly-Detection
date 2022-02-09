1bdf9cdbdcaaf9dd20dfa3798d321c59eff2d19f
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-09 21:44:39 +0100

    Optimizing code

--- api/stl.py
+++ api/stl.py
@@ -1,39 +1,32 @@
 from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+from io import StringIO
 
-from pipeline.preprocess import get_sensor_data, sample_sensor_data, get_indexed_df
-from pipeline.training import stl_model, get_anomaly_limits, get_anomalies
+from pipeline.preprocess import get_preprocessed, sample_sensor_data
+from pipeline.training import stl_model, get_anomalies, get_indexed_df, get_anomaly_limits
 
 
-stl = FastAPI()
+stl_decomposition = FastAPI()
 
 # Routes
-@stl.get("/")
-async def index():
-   return {"api_name": "STL Decomposition"}
-
-
-@stl.post("/STL/")
+@stl_decomposition.post("/")
 async def create_upload_file_stl(coef: str, sensor_data: UploadFile = File(...)):
    
-   file = sensor_data.filename
-   dfsensor, anomalies = stl_decomposition(sensor_data, int(coef))
-   anomalies = anomalies.rename({'0': 'sensor_values'}, axis=1).reset_index()
+   #read from csv
+   df = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
+   #preprocess data
+   dframe = get_preprocessed(df)
+   #define x
+   df = dframe.iloc[:, 0:1]
+
+   sampled_df = sample_sensor_data(df)
+   stlData = stl_model(sampled_df)
+   l, u = get_anomaly_limits(stlData.resid, coef)
+   anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
+   anomalies = anomalies.reset_index()
+   anomalies_list = anomalies.values.tolist()
+   dfsensor = get_indexed_df(sampled_df, 0)
    dfsensor = dfsensor.reset_index()
-   filepathAnomalies = "./data/uploads/stl.csv"
-   filepathSampledSensorData = "./data/uploads/sampledSensorStl.csv"
-   # to upload files
-   anomalies.to_csv(filepathAnomalies, index=False)
-   dfsensor.to_csv(filepathSampledSensorData, index=False)
+   dfsensor_list = dfsensor.values.tolist()
 
-   return {"anomaly_csv": filepathAnomalies, "sensor_csv": filepathSampledSensorData}
-
-
-def stl_decomposition(file, coeff):
-    df = get_sensor_data(file)
-    sampled_df = sample_sensor_data(df)
-    print(sampled_df)
-    stlData = stl_model(sampled_df)
-    l, u = get_anomaly_limits(stlData.resid, coeff)
-    anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
-    indexedSensor = get_indexed_df(sampled_df, 0)
-    return indexedSensor, anomalies
+   return {"anomalies_df_list": anomalies_list, "sensor_df_list": dfsensor_list}
