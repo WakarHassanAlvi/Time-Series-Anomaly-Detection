887dfdd2d38bf70121c0ea4542a700bdd93f6fbc
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-09 20:22:48 +0100

    Optimizing code

--- api/if.py
+++ api/if.py
@@ -3,15 +3,18 @@ import pandas as pd
 from io import StringIO
 import pickle
 
-
 from pipeline.preprocess import get_preprocessed, impute_missing
 
 ifr = FastAPI()
 
 # Routes
-@ifr.post("/")
+@ifr.get("/")
+async def index():
+   return {"api_name": "Isolation Forest"}
+
+@ifr.post("/IF")
 async def create_upload_file_if(sensor_data: UploadFile = File(...)):
-    #read from csv
+   #read from csv
    df = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
    #preprocess data
    dframe = get_preprocessed(df)
@@ -24,30 +27,10 @@ async def create_upload_file_if(sensor_data: UploadFile = File(...)):
    #make prediction
    y_pred = loaded_model.predict(X)
    predictions = y_pred.tolist()
-   print(type(predictions))
-   # res = pd.concat([X.reset_index(), pd.DataFrame(data=y_pred, columns=['PredictedAnamoly'])], axis=1)
-   # res['timestamp'] = pd.to_datetime(res['timestamp'])
-   # res = res.set_index('timestamp')
-
-   #res['PredictedAnamoly'] = res['PredictedAnamoly'].map(
-    #               {1:'1' , -1:'-1'})
-   #print(res['PredictedAnamoly'].value_counts())
-
-   # res = dframe.copy()
-   # res['PredictedAnamoly'] = predictions
-   # res['machine_status'] = dframe['machine_status']
+   
    df = impute_missing(df)
    df['PredictedAnamoly'] = predictions
 
-   # res['machine_status'] = res['machine_status'].astype(str)
-   # res['sensor_values'] = res['sensor_values'].astype(float)
-   # res['PredictedAnamoly'] = res['PredictedAnamoly'].astype(float)
-
    df_list = df.values.tolist()
-
-   # filepath = "./data/uploads/ifr.csv"
-   # res.to_csv(os.path.abspath(filepath), index=False)
-   
-   # return {"csv": os.path.abspath(filepath)}
 
    return {"anomalies_df": df_list}
--- api/lof.py
+++ api/lof.py
@@ -3,8 +3,6 @@ from git import typ
 import pandas as pd
 from io import StringIO
 import pickle
-import os
-
 
 from pipeline.preprocess import get_preprocessed, impute_missing
 
@@ -13,48 +11,27 @@ lof = FastAPI()
 # Routes
 @lof.get("/")
 async def index():
-   return {"api_name": "Isolation Forest"}
+   return {"api_name": "LOF"}
 
 
 @lof.post("/LOF")
 async def create_upload_file_lof(sensor_data: UploadFile = File(...)) -> pd.DataFrame:
-    #read from csv
+   #read from csv
    df = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
    #preprocess data
    dframe = get_preprocessed(df)
    #define x
    X = dframe.iloc[:, 0:1]
-   #X = X.dropna()
    
    #load model
    loaded_model = pickle.load(open('./models/model_lof.pkl', 'rb'))
    #make prediction
    y_pred = loaded_model.predict(X)
    predictions = y_pred.tolist()
-   print(type(predictions))
-   # res = pd.concat([X.reset_index(), pd.DataFrame(data=y_pred, columns=['PredictedAnamoly'])], axis=1)
-   # res['timestamp'] = pd.to_datetime(res['timestamp'])
-   # res = res.set_index('timestamp')
-
-   #res['PredictedAnamoly'] = res['PredictedAnamoly'].map(
-    #               {1:'1' , -1:'-1'})
-   #print(res['PredictedAnamoly'].value_counts())
-
-   # res = dframe.copy()
-   # res['PredictedAnamoly'] = predictions
-   # res['machine_status'] = dframe['machine_status']
+   
    df = impute_missing(df)
    df['PredictedAnamoly'] = predictions
 
-   # res['machine_status'] = res['machine_status'].astype(str)
-   # res['sensor_values'] = res['sensor_values'].astype(float)
-   # res['PredictedAnamoly'] = res['PredictedAnamoly'].astype(float)
-
    df_list = df.values.tolist()
-
-   # filepath = "./data/uploads/ifr.csv"
-   # res.to_csv(os.path.abspath(filepath), index=False)
-   
-   # return {"csv": os.path.abspath(filepath)}
 
    return {"anomalies_df": df_list}
