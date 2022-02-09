0031bf6ba3b7749951b58fac4d2679d0136d50f9
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-09 15:15:05 +0100

    changes for heroku

--- api/stl.py
+++ api/stl.py
@@ -3,7 +3,7 @@ import pandas as pd
 from io import StringIO
 
 from pipeline.preprocess import get_preprocessed, sample_sensor_data
-from pipeline.training import stl_model
+from pipeline.training import stl_model, get_anomalies, get_indexed_df, get_anomaly_limits
 
 
 stl_decomposition = FastAPI()
@@ -12,7 +12,6 @@ stl_decomposition = FastAPI()
 @stl_decomposition.post("/")
 async def create_upload_file_stl(coef: str, sensor_data: UploadFile = File(...)):
    
-   #df = get_sensor_data(sensor_data)
    #read from csv
    df = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
    #preprocess data
@@ -21,21 +20,20 @@ async def create_upload_file_stl(coef: str, sensor_data: UploadFile = File(...))
    df = dframe.iloc[:, 0:1]
 
    sampled_df = sample_sensor_data(df)
-   #print(sampled_df)
    stlData = stl_model(sampled_df)
-   print(stlData)
-   # # l, u = get_anomaly_limits(stlData.resid, coef)
-   # anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
-   # dfsensor = get_indexed_df(sampled_df, 0)
-
-   # #dfsensor, anomalies = stl_decomposition(sensor_data, int(coef))
-   # anomalies = anomalies.rename({'0': 'sensor_values'}, axis=1).reset_index()
-   # dfsensor = dfsensor.reset_index()
-   # filepathAnomalies = "./data/uploads/stl.csv"
-   # filepathSampledSensorData = "./data/uploads/sampledSensorStl.csv"
    
-   # # to upload files
-   # anomalies.to_csv(filepathAnomalies, index=False)
-   # dfsensor.to_csv(filepathSampledSensorData, index=False)
+   l, u = get_anomaly_limits(stlData.resid, coef)
+   anomalies = get_anomalies(stlData.resid, sampled_df, l, u)
+   dfsensor = get_indexed_df(sampled_df, 0)
 
-   # return {"anomaly_csv": filepathAnomalies, "sensor_csv": filepathSampledSensorData}
+   #dfsensor, anomalies = stl_decomposition(sensor_data, int(coef))
+   anomalies = anomalies.rename({'0': 'sensor_values'}, axis=1).reset_index()
+   dfsensor = dfsensor.reset_index()
+   filepathAnomalies = "./data/uploads/stl.csv"
+   filepathSampledSensorData = "./data/uploads/sampledSensorStl.csv"
+   
+   # to upload files
+   anomalies.to_csv(filepathAnomalies, index=False)
+   dfsensor.to_csv(filepathSampledSensorData, index=False)
+
+   return {"anomaly_csv": filepathAnomalies, "sensor_csv": filepathSampledSensorData}
