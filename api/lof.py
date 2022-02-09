ed246fd48b18cde8d6600b0b614a6d4ef3be75b9
Waqar Hassan Alvi <wakar.hassan.alvi@gmail.com>
2022-02-09 21:48:28 +0100

    Optimizing code

--- api/lof.py
+++ api/lof.py
@@ -3,42 +3,29 @@ import pandas as pd
 from io import StringIO
 import pickle
 
-
-from pipeline.preprocess import get_preprocessed
+from pipeline.preprocess import get_preprocessed, impute_missing
 
 lof = FastAPI()
 
 # Routes
-@lof.get("/")
-async def index():
-   return {"api_name": "LOF"}
-
-@lof.post("/LOF/")
+@lof.post("/")
 async def create_upload_file_lof(sensor_data: UploadFile = File(...)) -> pd.DataFrame:
     #read from csv
-   dframe = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
+   df = pd.read_csv(StringIO(str(sensor_data.file.read(), 'utf-8')), encoding='utf-8')
    #preprocess data
-   dframe = get_preprocessed(dframe)
+   dframe = get_preprocessed(df)
    #define x
    X = dframe.iloc[:, 0:1]
-   #X = X.dropna()
    
    #load model
    loaded_model = pickle.load(open('./models/model_lof.pkl', 'rb'))
    #make prediction
    y_pred = loaded_model.predict(X)
+   predictions = y_pred.tolist()
+   
+   df = impute_missing(df)
+   df['PredictedAnamoly'] = predictions
 
-   res = pd.concat([X.reset_index(), pd.DataFrame(data=y_pred, columns=['PredictedAnamoly'])], axis=1)
-   res['timestamp'] = pd.to_datetime(res['timestamp'])
-   res = res.set_index('timestamp')
+   df_list = df.values.tolist()
 
-   res['PredictedAnamoly'] = res['PredictedAnamoly'].map(
-                   {1:'1' , -1:'-1'})
-   #print(res['PredictedAnamoly'].value_counts())
-
-   res['machine_status'] = dframe['machine_status']
-   print(res)
-
-   filepath = "./data/uploads/lof.csv"
-   res.to_csv(filepath, index=False)
-   return {"csv": filepath}
+   return {"anomalies_df": df_list}
